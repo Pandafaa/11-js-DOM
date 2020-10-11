ecbb6a32690aa453ddc907683568d4a5ca352360
astrophysics <vmir88@gmail.com>
2020-10-12 00:24:45 +0300

    fixed2

--- animation.js
+++ animation.js
@@ -4,33 +4,24 @@ const container = document.getElementById('container');
 let pos = 1;
 let x = 1;
 let y = 1;
-setInterval(move, 10);
+setInterval(move, 5);
 
 let width = container.clientWidth-box.clientWidth;
 let height = container.clientHeight-box.clientHeight;
 
-console.log(width)
-
-let lu = false;
-let ld = false;
-let ru = false;
-let rd = true;
-
-// let left = false;
 let right = true;
-// let up = false;
 let down = true;
 
 function move() {
     box.style.left = x + 'px';
     box.style.top = y + 'px';
-    x = right?x+1:x-1;
+    x = right?x+2:x-2;
     y = down?y+1:y-1;
 
-    if(x==width+1 || x==0) {
+    if(x>=width+1 || x<=0) {
         right=!right;
     }
-    if(y==height+1 || y==0) {
+    if(y>=height+1 || y<=0) {
         down=!down;
     }
 }
